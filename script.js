20ba601280de367f496104af5a6b2f0231a5f115
Eugene <zuevevgen93@mail.ru>
2022-06-29 22:09:06 +0300

    Моя маленькая игра

---
+++ script.js
@@ -0,0 +1,102 @@
+const startBtn = document.querySelector("#start");
+const screens = document.querySelectorAll(".screen");
+const timeList = document.querySelector("#time-list");
+const timeElement = document.querySelector("#time");
+const board = document.querySelector("#board");
+const buttonUp = document.querySelector(".button-up");
+const colors = [
+  "#d4531d",
+  "#e1c133",
+  "#49dc1e",
+  "#19e4d4",
+  "#14a3e8",
+  "#1719e9",
+  "#7b1be8",
+  "#e31fc2",
+  "#e41414",
+  "#8446f7",
+  "#30c7ec",
+  "#c4e316",
+];
+
+let time = 0;
+let score = 0;
+startBtn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  screens[0].classList.remove("back");
+  screens[0].classList.add("up");
+});
+
+timeList.addEventListener("click", (evt) => {
+  if (evt.target.classList.contains("time-btn")) {
+    time = parseInt(evt.target.getAttribute("data-time"));
+    screens[1].classList.add("up");
+    startGame();
+  }
+});
+
+board.addEventListener("click", (evt) => {
+  if (evt.target.classList.contains("circle")) {
+    score++;
+    evt.target.remove();
+    createRandomCircle();
+  }
+});
+
+function startGame() {
+  setInterval(decreaseTime, 1000);
+  createRandomCircle();
+  setTime(time);
+}
+
+function decreaseTime() {
+  if (time === 0) {
+    finishGame();
+  } else {
+    let current = --time;
+    if (current < 10) {
+      current = `0${current}`;
+    }
+    setTime(current);
+  }
+}
+
+function setTime(value) {
+  timeElement.innerHTML = `00:${value}`;
+}
+
+function finishGame() {
+  timeElement.parentNode.classList.add("hide");
+  board.innerHTML = `<div class="end-show"><h1>Натыкано: <span class="primary">${score}</span></h1><button class="restart" value="Refresh Page" onClick="window.location.reload();">Заново</button></div>`;
+}
+
+function createRandomCircle() {
+  const circle = document.createElement("div");
+  const size = getRandomNumber(10, 60);
+  const { width, height } = board.getBoundingClientRect();
+  const x = getRandomNumber(0, width - size);
+  const y = getRandomNumber(0, height - size);
+
+  circle.classList.add("circle");
+  circle.style.width = `${size}px`;
+  circle.style.height = `${size}px`;
+  circle.style.right = `${x}px`;
+  circle.style.top = `${y}px`;
+  circle.style.background = getRandColor();
+  board.append(circle);
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+buttonUp.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  screens[0].classList.remove("up");
+  screens[0].classList.add("back");
+});
+
+function getRandColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
